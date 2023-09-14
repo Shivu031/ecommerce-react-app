06d796895406ff6ee72a314b175a560143535734
Shivu031 <128800786+Shivu031@users.noreply.github.com>
2023-09-14 16:21:48 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "girl.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "winter.jpg",
+      title: "WINTER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "girl_bg.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "shirt.jpg",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "winter.jpg",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "jacket.jpg",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"frock.jpg",
+    },
+    {
+      id:2,
+      img:"t-shirt2.jpg",
+    },
+    {
+      id:3,
+      img:"top.jpg",
+    },
+    {
+      id:4,
+      img:"m_tshirt.jpg",
+    },
+    {
+      id:5,
+      img:"t-shirt.jpg",
+    },
+    {
+      id:6,
+      img:"cap.jpg",
+    },
+    {
+      id:7,
+      img:"jacket2.jpg",
+    },
+    {
+      id:8,
+      img:"bag.jpg",
+    },
+  ]
